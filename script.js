f3b9bdb260a9a26a97a32ca2fa3a93a024fb5161
mintesGeb <73610057+mintesGeb@users.noreply.github.com>
2021-02-23 10:21:59 -0500

    JS - start Html - button added Style- updated

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // alert("Books yay!");
+
+// selector
+const addBooksBtn = document.querySelector(".button-add");
+
+addBooksBtn.addEventListener("click", function () {
+  let x = prompt("What book did you read?");
+});
