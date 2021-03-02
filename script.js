78b7c74f88c82a7cc9fc1f8f1aedfcb1161d30e7
mintesGeb <73610057+mintesGeb@users.noreply.github.com>
2021-03-02 11:31:07 -0500

    update

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, function () {
+  console.log("server has started in port 3000 ");
+});
+
 // alert("Books yay!");
 
 // selector
-const addBooksBtn = document.querySelector(".button-add");
+// const addBooksBtn = document.querySelector(".button-add");
+//
+// addBooksBtn.addEventListener("click", function () {
+//   let x = prompt("What book did you read?");
+// tableCreate();
+// });
 
-addBooksBtn.addEventListener("click", function () {
-  let x = prompt("What book did you read?");
-});
+// how to dynamically add/remove tables
+
+const form = document.querySelector("")
